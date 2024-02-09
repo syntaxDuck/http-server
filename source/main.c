28256942d022e6419cbd905369867dc20a7f2682
syntaxDuck <kam.comer96@gmail.com>
2024-02-09 00:20:48 -0500

    Finished basic server port opening

--- source/main.c
+++ source/main.c
@@ -12,10 +12,11 @@
 int main() {
   int server_fd, new_socket;
   struct sockaddr_in address;
+  int addresslen = sizeof(address);
 
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = PORT;
+  address.sin_port = htons(PORT);
 
   int opt = 1;
 
@@ -26,4 +27,21 @@ int main() {
   if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
     exit(EXIT_FAILURE);
   }
+
+  if (listen(sockfd, 1) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Listening at on port %d\n", PORT);
+
+  int client;
+  if ((client = accept(sockfd, (struct sockaddr *)&address,
+                       (socklen_t *)&addresslen)) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  write(client, "Hello World!\n", 18);
+  printf("Hellow World sent to client\n");
+  close(client);
+  exit(EXIT_SUCCESS);
 }
